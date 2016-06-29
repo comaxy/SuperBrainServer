66ace710289b23c376d925ec80662d73d8937d7a
Comaxy <comaxy@126.com>
2016-06-29 16:43:00 +0800

    增加数据库类

--- SuperBrainServer/DbManager.cpp
+++ SuperBrainServer/DbManager.cpp
@@ -1,16 +1,249 @@
 #include "DbManager.h"
+#include "StringUtil.h"
+#include "sqlite/sqlite3.h"
+#include <atlstr.h>
 
-bool DbManager::initialize()
+Sqlite::Sqlite()
+	: m_connection(nullptr)
 {
-	//// 初始化数据库
-	//CString dbPath = exeFilePath.Left(exeFilePath.ReverseFind(TEXT('\\')) + 1) + TEXT("sb.db");
-	//sqlite3* db = NULL;
-	//sqlite3_open(StringUtil::CStringToUtf8(dbPath).c_str(), &db);
+}
+
+Sqlite::~Sqlite()
+{
+	close();
+}
+
+void Sqlite::close()
+{
+	if (m_connection)
+	{
+		sqlite3_close(m_connection);
+		m_connection = nullptr;
+	}
+}
+
+bool Sqlite::initialize(const std::string& path)
+{
+	int ret = sqlite3_open(path.c_str(), &m_connection);
+	if (ret != SQLITE_OK)
+	{
+		return false;
+	}
 
 	return true;
 }
 
+void Sqlite::uninitialize()
+{
+	close();
+}
+
+std::shared_ptr<SqliteQuery> Sqlite::makeQuery()
+{
+	return std::make_shared<SqliteQuery>(this, m_connection);
+}
+
+SqliteQuery::SqliteQuery(Sqlite* database, sqlite3* connection)
+	: m_connection(connection)
+	, m_stmt(nullptr)
+	, m_result()
+	, m_iter()
+	, m_fetchFirstResult(false)
+{
+}
+
+bool SqliteQuery::exec(const CString& sql)
+{
+	if (prepare(sql))
+	{
+		return exec();
+	}
+	else
+	{
+		return false;
+	}
+}
+
+Variant SqliteQuery::getValueFromColumn(sqlite3_stmt* stmt, int column)
+{
+	int sqlite3_type = sqlite3_column_type(stmt, column);
+	switch (sqlite3_type)
+	{
+	case SQLITE_INTEGER:
+		return Variant(sqlite3_column_int(stmt, column));
+	case SQLITE_FLOAT:
+		return Variant(sqlite3_column_double(stmt, column));
+	case SQLITE_TEXT:
+		return Variant(
+			StringUtil::Utf8ToCString(std::string(
+			reinterpret_cast<const char*>(sqlite3_column_text(stmt, column)),
+			sqlite3_column_bytes(stmt, column))));
+	case SQLITE_BLOB:
+		return Variant(
+			reinterpret_cast<const char*>(sqlite3_column_blob(stmt, column)),
+			sqlite3_column_bytes(stmt, column));
+	default:
+		break;
+	}
+
+	return Variant();
+}
+
+bool SqliteQuery::prepare(const CString& sql)
+{
+	m_result.clear();
+	m_stmt = nullptr;
+
+	int ret = sqlite3_prepare_v2(m_connection,
+		StringUtil::CStringToUtf8(sql).c_str(), -1,
+		&m_stmt, nullptr);
+	if (ret != SQLITE_OK)
+	{
+		sqlite3_finalize(m_stmt);
+		return false;
+	}
+
+	return true;
+}
+
+void SqliteQuery::bindValue(const CString& key, const Variant& value)
+{
+	int index = sqlite3_bind_parameter_index(m_stmt, StringUtil::CStringToUtf8(key).c_str());
+	bindValue(index, value);
+}
+
+void SqliteQuery::bindValue(int i, const Variant& value)
+{
+	switch (value.type())
+	{
+	case Variant::TYPE_INT:
+		sqlite3_bind_int(m_stmt, i, value.toInt());
+		break;
+	case Variant::TYPE_DOUBLE:
+		sqlite3_bind_double(m_stmt, i, value.toDouble());
+		break;
+	case Variant::TYPE_TEXT:
+		{
+			CString text = value.toString();
+			std::string utf8Text = StringUtil::CStringToUtf8(text);
+			sqlite3_bind_text(m_stmt, i, utf8Text.c_str(), utf8Text.size(), SQLITE_TRANSIENT);
+		}
+		break;
+	case Variant::TYPE_BLOB:
+		{
+			std::pair<char*, unsigned int> buffer = value.toBlob();
+			sqlite3_bind_blob(m_stmt, i, buffer.first, buffer.second, SQLITE_TRANSIENT);
+		}
+		break;
+	default:
+		break;
+	}
+}
+
+bool SqliteQuery::exec()
+{
+	int ret = sqlite3_step(m_stmt);
+	while (ret == SQLITE_ROW)
+	{
+		Row row;
+		int columnCount = sqlite3_column_count(m_stmt);
+		for (int column = 0; column < columnCount; ++column)
+		{
+			RowItem rowItem;
+			rowItem.key = StringUtil::Utf8ToCString(sqlite3_column_name(m_stmt, column));
+			rowItem.value = getValueFromColumn(m_stmt, column);
+			row.push_back(rowItem);
+		}
+		m_result.push_back(row);
+		ret = sqlite3_step(m_stmt);
+	}
+
+	sqlite3_finalize(m_stmt);
+	m_stmt = nullptr;
+
+	if (!m_result.empty())
+	{
+		m_fetchFirstResult = true;
+	}
+
+	return true;
+}
+
+bool SqliteQuery::next()
+{
+	if (m_result.empty())
+	{
+		return false;
+	}
+
+	if (m_fetchFirstResult)
+	{
+		m_iter = m_result.begin();
+		m_fetchFirstResult = false;
+	}
+	else
+	{
+		++m_iter;
+	}
+
+	if (m_iter == m_result.end())
+	{
+		return false;
+	}
+	else
+	{
+		return true;
+	}
+}
+
+Variant SqliteQuery::value(const CString& key)
+{
+	for (auto iterItem = m_iter->begin(); iterItem != m_iter->end(); ++iterItem)
+	{
+		if (iterItem->key == key)
+		{
+			return iterItem->value;
+		}
+	}
+	return Variant();
+}
+
+Variant SqliteQuery::value(unsigned int i)
+{
+	if (i >= m_iter->size() || i < 0)
+	{
+		return Variant();
+	}
+
+	return (*m_iter)[i].value;
+}
+
+bool DbManager::initialize()
+{
+	CString exeFilePath;
+	GetModuleFileName(NULL, exeFilePath.GetBufferSetLength(MAX_PATH), MAX_PATH);
+	CString dbPath = exeFilePath.Left(exeFilePath.ReverseFind(TEXT('\\')) + 1) + TEXT("sb.db");
+	if (!m_sqlite->initialize(StringUtil::CStringToUtf8(dbPath)))
+	{
+		return false;
+	}
+
+	std::shared_ptr<SqliteQuery> query = m_sqlite->makeQuery();
+	bool ok = false;
+	ok = query->exec(CString(
+		"CREATE TABLE IF NOT EXISTS "
+		"player ("
+		"name                  TEXT,"
+		"password              TEXT,"
+		"PRIMARY KEY (name)"
+		")"));
+	if (!ok)
+	{
+		return false;
+	}
+}
+
 void DbManager::uninitialize()
 {
-
+	m_sqlite->uninitialize();
 }
--- SuperBrainServer/DbManager.h
+++ SuperBrainServer/DbManager.h
@@ -1,6 +1,157 @@
+#include "sqlite/sqlite3.h"
+#include <atlstr.h>
+#include <memory>
+#include <vector>
+
+class SqliteQuery;
+
+class Sqlite
+{
+public:
+	Sqlite();
+	virtual ~Sqlite();
+	virtual bool initialize(const std::string& path);
+	virtual void uninitialize();
+	virtual std::shared_ptr<SqliteQuery> makeQuery();
+private:
+	void close();
+private:
+	sqlite3* m_connection;
+};
+
+class Variant
+{
+public:
+	enum Type
+	{
+		TYPE_UNKNOWN,
+		TYPE_INT,
+		TYPE_DOUBLE,
+		TYPE_TEXT,
+		TYPE_BLOB,
+	};
+
+public:
+	Variant() { m_type = TYPE_UNKNOWN; }
+	Variant(int intValue) { init();  m_type = TYPE_INT; m_intValue = intValue; }
+	Variant(double doubleValue) { init();  m_type = TYPE_DOUBLE; m_doubleValue = doubleValue; }
+	Variant(const CString& textValue) { init(); m_type = TYPE_TEXT; m_textValue = textValue; }
+	Variant(const char* buffer, unsigned int size)
+	{
+		init();
+		m_type = TYPE_BLOB;
+		m_blobValue = new char[size]();
+		memcpy(m_blobValue, buffer, size);
+		m_blobSize = size;
+	}
+
+	Variant(const Variant& variant)
+	{
+		copy(variant);
+	}
+	const Variant& operator = (const Variant& variant)
+	{
+		copy(variant);
+		return *this;
+	}
+
+	Type type() const { return m_type; }
+
+	int toInt() const { return m_intValue; }
+	double toDouble() const { return m_doubleValue; }
+	CString toString() const { return m_textValue; }
+	std::pair<char*, unsigned int> toBlob() const { return std::make_pair(m_blobValue, m_blobSize); }
+
+private:
+	void init()
+	{
+		m_type = TYPE_UNKNOWN;
+		m_intValue = 0;
+		m_doubleValue = 0.0;
+		m_textValue = TEXT("");
+		m_blobValue = nullptr;
+	}
+	void copy(const Variant& variant)
+	{
+		switch (variant.m_type)
+		{
+		case TYPE_INT:
+			m_type = TYPE_INT;
+			m_intValue = variant.m_intValue;
+			break;
+		case TYPE_DOUBLE:
+			m_type = TYPE_DOUBLE;
+			m_doubleValue = variant.m_doubleValue;
+			break;
+		case TYPE_TEXT:
+			m_type = TYPE_TEXT;
+			m_textValue = variant.m_textValue;
+			break;
+		case TYPE_BLOB:
+			m_type = TYPE_BLOB;
+			m_blobValue = new char[variant.m_blobSize]();
+			memcpy(m_blobValue, variant.m_blobValue, variant.m_blobSize);
+			m_blobSize = variant.m_blobSize;
+			break;
+		case TYPE_UNKNOWN:
+		default:
+			break;
+		}
+	}
+
+private:
+
+	Type m_type;
+	int m_intValue;
+	double m_doubleValue;
+	CString m_textValue;
+	char* m_blobValue;
+	unsigned int m_blobSize;
+};
+
+class SqliteQuery
+{
+	struct RowItem
+	{
+		CString key;
+		Variant value;
+	};
+
+	typedef std::vector<RowItem> Row;
+	typedef std::vector<Row> Result;
+
+public:
+	SqliteQuery(Sqlite* database, sqlite3* connection);
+	virtual ~SqliteQuery() {}
+	virtual bool exec(const CString& sql);
+	virtual bool prepare(const CString& sql);
+	virtual void bindValue(const CString& key, const Variant& value);
+	virtual void bindValue(int i, const Variant& value);
+	virtual bool exec();
+	virtual bool next();
+	virtual Variant value(const CString& key);
+	virtual Variant value(unsigned int i);
+
+private:
+	Variant getValueFromColumn(sqlite3_stmt* stmt, int column);
+
+private:
+	sqlite3* m_connection;
+	sqlite3_stmt* m_stmt;
+	Result m_result;
+	Result::const_iterator m_iter;
+	bool m_fetchFirstResult;
+};
+
 class DbManager
 {
 public:
+	DbManager() : m_sqlite(new Sqlite()) {}
 	bool initialize();
 	void uninitialize();
+
+	std::shared_ptr<Sqlite> sqlite() const { return m_sqlite; }
+
+private:
+	std::shared_ptr<Sqlite> m_sqlite;
 };
